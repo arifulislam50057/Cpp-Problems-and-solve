603ba7ce775b5d7902e7b943069d52890db4eea8
arifulislam50057 <arifulislam50057@gmail.com>
2024-04-03 20:00:24 +0600

    prob-1

---
+++ input-output-datatype.cpp
@@ -0,0 +1,28 @@
+//1.Write a program to perform input/output of all basic data types.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    float f;
+    double d;
+    char c;
+    string s;
+    bool b;
+    cin>>i>>f>>d>>c>>s>>b;
+    cout<<i<<endl;
+    cout<<fixed<<setprecision(3)<<f<<endl;
+    cout<<fixed<<setprecision(9)<< d<<endl;
+    cout<<c<<endl<<s<<endl<<b<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
