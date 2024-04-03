00550f2323dfaba1ee29b2274eaaaad110977498
arifulislam50057 <arifulislam50057@gmail.com>
2024-04-03 20:29:12 +0600

    prob-3

--- input-output-datatype.cpp
+++ input-output-datatype.cpp
@@ -27,6 +27,24 @@ int main(){
     return 0;
 }
 
+//3.Write a program to enter two numbers and perform all arithmetic operations.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float a ,b;
+    cin >> a >>b;
+    float sum, mul, div, subtr;
+    sum = a + b;
+    cout <<"a+b: "<< fixed << setprecision(3) << sum <<endl;
+    mul=a*b;
+    cout <<"a*b: "<< fixed << setprecision(3) << mul <<endl;
+    div=a/b;
+    cout <<"a/b: "<< fixed << setprecision(3) << div <<endl;
+    subtr=a-b;
+    cout <<"a-b: "<< fixed << setprecision(3) << subtr <<endl;
+    
+    return 0;
+}
 
 
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    float a , b;
-    cin >>a >>b;
-    cout<< "the sum is:"<<ends;
-    cout<<fixed<<setprecision(4)<< a+b <<endl;
+    float a ,b;
+    cin >> a >>b;
+    float sum, mul, div, subtr;
+    sum = a + b;
+    cout <<"a+b: "<< fixed << setprecision(3) << sum <<endl;
+    mul=a*b;
+    cout <<"a*b: "<< fixed << setprecision(3) << mul <<endl;
+    div=a/b;
+    cout <<"a/b: "<< fixed << setprecision(3) << div <<endl;
+    subtr=a-b;
+    cout <<"a-b: "<< fixed << setprecision(3) << subtr <<endl;
+    
     return 0;
 }
