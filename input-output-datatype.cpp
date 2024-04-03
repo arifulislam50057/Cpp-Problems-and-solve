b4422a80dba2f8b27cdd5ff724d02b0df5abcbf4
arifulislam50057 <arifulislam50057@gmail.com>
2024-04-03 20:38:37 +0600

    prob-4

--- input-output-datatype.cpp
+++ input-output-datatype.cpp
@@ -46,6 +46,21 @@ int main(){
     return 0;
 }
 
+//4.Write a program to enter the length and width of a rectangle and find its area and perimeter
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float length , width , area, perimeter;
+    cout<<"enter the length: ";
+    cin>>length;
+    cout<<"enter the width: ";
+    cin>>width;
+    area= length*width;
+    perimeter= 2*(length+width);
+    cout<<"area: "<<fixed<<setprecision(4)<<area<<endl;
+    cout<<"perimeter: "<< fixed<<setprecision(4)<<perimeter<< endl;
+    return 0;
+}
 
 
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,17 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    float a ,b;
-    cin >> a >>b;
-    float sum, mul, div, subtr;
-    sum = a + b;
-    cout <<"a+b: "<< fixed << setprecision(3) << sum <<endl;
-    mul=a*b;
-    cout <<"a*b: "<< fixed << setprecision(3) << mul <<endl;
-    div=a/b;
-    cout <<"a/b: "<< fixed << setprecision(3) << div <<endl;
-    subtr=a-b;
-    cout <<"a-b: "<< fixed << setprecision(3) << subtr <<endl;
-    
+    float length , width , area, perimeter;
+    cout<<"enter the length: ";
+    cin>>length;
+    cout<<"enter the width: ";
+    cin>>width;
+    area= length*width;
+    perimeter= 2*(length+width);
+    cout<<"area: "<<fixed<<setprecision(4)<<area<<endl;
+    cout<<"perimeter: "<< fixed<<setprecision(4)<<perimeter<< endl;
     return 0;
 }
