c9b5bce2c216c8b8f296bc9e02eba07012c92852
arifulislam50057 <arifulislam50057@gmail.com>
2024-04-03 20:52:39 +0600

    prob-5

--- input-output-datatype.cpp
+++ input-output-datatype.cpp
@@ -62,6 +62,23 @@ int main(){
     return 0;
 }
 
+//5.Write a program to enter the radius of a circle and find its diameter, circumference and area.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float r , d, c, a;
+    cout<<"enter the radius: ";
+    cin >> r;
+    d= 2*r;
+    cout<<"diameter: "<<fixed<< setprecision(4)<<d<<endl;
+    c=2* M_PI * r;
+    cout << "circumference: "<<fixed<< setprecision(4)<<c<<endl;
+    a=M_PI * pow(r,2);
+    cout<<"area: "<<fixed<< setprecision(4)<<a<<endl;
+    return 0;
+
+}
+
 
 
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    float length , width , area, perimeter;
-    cout<<"enter the length: ";
-    cin>>length;
-    cout<<"enter the width: ";
-    cin>>width;
-    area= length*width;
-    perimeter= 2*(length+width);
-    cout<<"area: "<<fixed<<setprecision(4)<<area<<endl;
-    cout<<"perimeter: "<< fixed<<setprecision(4)<<perimeter<< endl;
+    float r , d, c, a;
+    cout<<"enter the radius: ";
+    cin >> r;
+    d= 2*r;
+    cout<<"diameter: "<<fixed<< setprecision(4)<<d<<endl;
+    c=2* M_PI * r;
+    cout << "circumference: "<<fixed<< setprecision(4)<<c<<endl;
+    a=M_PI * pow(r,2);
+    cout<<"area: "<<fixed<< setprecision(4)<<a<<endl;
     return 0;
+
 }
