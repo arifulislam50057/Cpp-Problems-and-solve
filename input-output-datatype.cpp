366800aedd66d8c488096d9fd4d3ccddd57df615
arifulislam50057 <arifulislam50057@gmail.com>
2024-04-05 00:39:08 +0600

    prob-6

--- input-output-datatype.cpp
+++ input-output-datatype.cpp
@@ -79,6 +79,20 @@ int main(){
 
 }
 
+//6.Write a program to enter temperature in Celsius and convert it into Fahrenheit.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float tempCelcius , tempFahrenheit;
+    cin>>tempCelcius;
+    tempFahrenheit=((1.8)*(tempCelcius))+32;
+    cout<<fixed<<setprecision(3)<<tempFahrenheit<<endl;     
+    return 0;
+}
+
+
+
+
 
 
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    float r , d, c, a;
-    cout<<"enter the radius: ";
-    cin >> r;
-    d= 2*r;
-    cout<<"diameter: "<<fixed<< setprecision(4)<<d<<endl;
-    c=2* M_PI * r;
-    cout << "circumference: "<<fixed<< setprecision(4)<<c<<endl;
-    a=M_PI * pow(r,2);
-    cout<<"area: "<<fixed<< setprecision(4)<<a<<endl;
+    float tempCelcius , tempFahrenheit;
+    cin>>tempCelcius;
+    tempFahrenheit=((1.8)*(tempCelcius))+32;
+    cout<<fixed<<setprecision(3)<<tempFahrenheit<<endl;     
     return 0;
-
 }
